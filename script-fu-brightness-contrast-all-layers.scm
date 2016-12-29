127c4f0fd14b4f87422d07ea1dd83709f5914617
arthur-noseda <anoseda.dev@gmail.com>
2016-12-29 22:54:33 +0100

    Adjust brightness and contrast for all layers

---
+++ script-fu-brightness-contrast-all-layers.scm
@@ -0,0 +1,38 @@
+(define (script-fu-brightness-contrast-all-layers inImage inBrightness inContrast)
+  (let*
+    (
+      (layers (gimp-image-get-layers inImage))
+      (num-layers (car layers))
+      (layer-ids (cadr layers))
+      (i 0)
+    )
+    (gimp-image-undo-group-start inImage)
+    (while (< i num-layers)
+      ;(gimp-message (string-append "i: " (number->string i)))
+      (let*
+        ((layer (aref layer-ids i)))
+        ;(gimp-message (string-append "layer: " (number->string layer)))
+        (gimp-brightness-contrast layer inBrightness inContrast)
+      )
+      (set! i (+ i 1))
+    )
+    (gimp-image-undo-group-end inImage)
+    (gimp-displays-flush)
+  ))
+
+(script-fu-register
+   "script-fu-brightness-contrast-all-layers"  ;func name
+   "Brightness-Contrast-All-Layers..."         ;menu label
+   "Modify brightness/contrast for all layers\
+    of the image."                             ;description
+   "Arthur Noseda"                             ;author
+   "copyright 2016, Arthur Noseda"             ;copyright notice
+   "December 29, 2016"                         ;date created
+   ""                     ;image type that the script works on
+   SF-IMAGE       "Image"         1
+   SF-ADJUSTMENT  "Brightness:"   '(0 -127 127 1 10 0 SF-SLIDER)
+                                               ;a spin-button
+   SF-ADJUSTMENT  "Contrast"      '(0 -127 127 1 10 0 SF-SLIDER)
+                                               ;a spin-button
+ )
+ (script-fu-menu-register "script-fu-brightness-contrast-all-layers" "<Image>/Colors")
